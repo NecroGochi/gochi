ab49e932f2e33d9ffa1b3ac83a6e79cf34f0020e
NecroGochi <necro.gochi@gmail.com>
2023-08-08 12:57:00 +0200

    delete - class LobbyMeny and unnecessary comments
    The class was not used.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygame
 from Configure.configure import load_configure_data
 from Menu.main_menu import MainMenu
-from Menu.lobby_menu import LobbyMenu
-from Menu.options_menu import OptionsMenu
 from Configure.language_config import *
 from game import game
 from campaign import running_campaign
@@ -10,7 +8,6 @@ from campaign import running_campaign
 # Initialize Pygame
 pygame.init()
 
-# Set the window size
 # Set the window size
 window_width = 192 * 5
 window_height = 108 * 5
@@ -68,8 +65,6 @@ selected_option = 0
 
 # Menus
 main_menu = MainMenu(main_menu_options, window_width, window_height, font_title, font_options)
-lobby_menu = LobbyMenu(lobby_menu_options, window_width, window_height, font_title, font_options)
-#options_menu = OptionsMenu(options_menu_options, window_width, window_height, font_title, font_options)
 
 # Game loop
 running = True
@@ -83,13 +78,6 @@ while running:
         main_menu.render_version()
         main_menu.update_display()
 
-    '''
-    if menu_state == 'lobby':
-        menu_state = lobby_menu.events_handler(menu_state)
-        lobby_menu.clear_screen(black)
-        lobby_menu.render_options(white)
-        lobby_menu.update_display()
-    '''
     if menu_state == "Free_play":
         menu_state = game(menu_state)
         menu_state = 'main'
@@ -102,40 +90,3 @@ while running:
         images = ['dream_TradingCard(3).jpg', 'dream_TradingCard(4).jpg']
         if menu_state == 'end':
             menu_state = running_campaign(menu_state, 'Scenario\\scenario_02_pl.txt', image_path, images)
-
-    '''if menu_state == 'options':
-        language = options_menu.events_handler()
-
-        if language == "english":
-            # Set menu options
-            main_menu.update_menu_options(ENGLISH_MAIN_MENU_OPTIONS)
-            lobby_menu.update_menu_options([
-                configure_data["player_name"],
-                ENGLISH_LOBBY_MENU_OPTIONS[0],
-                ENGLISH_LOBBY_MENU_OPTIONS[1],
-                ENGLISH_LOBBY_MENU_OPTIONS[2]
-            ])
-            options_menu.update_menu_options([
-                configure_data["player_name"],
-                configure_data["language"],
-                ENGLISH_OPTIONS_MENU_OPTIONS[0]
-            ])
-            lobby_menu.change_the_language_of_character_class_name_to_english()
-        else:
-            # Set menu options
-            main_menu.update_menu_options(POLISH_MAIN_MENU_OPTIONS)
-            lobby_menu.update_menu_options([
-                configure_data["player_name"],
-                POLISH_LOBBY_MENU_OPTIONS[0],
-                POLISH_LOBBY_MENU_OPTIONS[1],
-                POLISH_LOBBY_MENU_OPTIONS[2]
-            ])
-            options_menu.update_menu_options([
-                configure_data["player_name"],
-                configure_data["language"],
-                POLISH_OPTIONS_MENU_OPTIONS[0]
-            ])
-            lobby_menu.change_the_language_of_character_class_name_to_polish()
-        options_menu.clear_screen()
-        options_menu.render_options()
-        options_menu.update_display()'''
