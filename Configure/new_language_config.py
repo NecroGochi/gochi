5763df0588ab2d86f5a4d56d1bb0e9534d6844fb
NecroGochi <necro.gochi@gmail.com>
2023-08-09 15:11:46 +0200

    add new variables at new_languages_config
    In laguages added: campaign, exit, game, options

--- Configure/new_language_config.py
+++ Configure/new_language_config.py
@@ -1,12 +1,20 @@
 languages = {
     'english': {
         'back': 'Back',
+        'campaign': 'Start campaign',
+        'exit': 'Exit',
+        'game': 'Start Game',
         'language': 'Language',
+        'options': 'Options',
         'player_name': 'Player'
     },
     'polish': {
         'back': 'Powrót',
+        'campaign': 'Rozpocznij kampanię',
+        'exit': 'Wyjście',
+        'game': 'Rozpocznij grę',
         'language': 'Język',
+        'options': 'Opcje',
         'player_name': 'Gracz'
     }
 }
