7d4d5ce904a2e379a95da5778ac5d5cad7b38151
NecroGochi <necro.gochi@gmail.com>
2023-08-22 14:46:34 +0200

    change variables names at Boss class
    The names was renamed like:
    ap -> attack
    dp -> defense
    hp -> health_points / health
    actual_hp -> actual_health_points
    exp -> experience

--- sprites/creatures/boss.py
+++ sprites/creatures/boss.py
@@ -2,18 +2,20 @@ from sprites.creatures.enemy import *
 
 
 class Boss(Enemy):
-    def __init__(self, position_x, position_y, size, color, speed, ap, dp, hp, exp, image):
+    health_point_y_bar_size = 10
+
+    def __init__(self, position_x, position_y, size, color, speed, attack, defense, health, experience, image):
         # Set player properties
         self.size = size
         self.image = color
         self.speed = speed
         self.hit = True
-        self.ap = ap
-        self.dp = dp
-        self.hp = hp
-        self.actual_hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.health_points = health
+        self.actual_health_points = health
         self.boss = True
-        self.exp = exp
+        self.experience = experience
         self.final_boss = False
 
         self.image_sprite = pygame.image.load(image)
@@ -26,7 +28,7 @@ class Boss(Enemy):
         pygame.draw.rect(window, color,
                          (self.hitbox.x - board_camera_x,
                           self.hitbox.y + self.size - board_camera_y,
-                          self.actual_hp * self.size / self.hp, 10))
+                          self.actual_health_points * self.size / self.health_points, self.health_point_y_bar_size))
 
     def is_final_boss(self):
         self.final_boss = True
