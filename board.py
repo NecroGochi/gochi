9be57e0ec727c045cee67eed21119788f3c929d9
NecroGochi <necro.gochi@gmail.com>
2023-08-22 14:54:36 +0200

    rename variebles at Charater class
    The veriables was renamed like:
    -board_border_x1 -> down_board_border
    -board_border_x2 ->up_board_border
    -board_border_y1 -> right_board_border
    -board_border_y2 -> left_board_border

--- board.py
+++ board.py
@@ -2,12 +2,13 @@ import pygame
 
 
 class Board:
-    def __init__(self, window_width, window_height, border_x1, border_y1, border_x2, border_y2):
     centering = {
         'x': 960,
         'y': 540
     }
 
+    def __init__(self, window_width, window_height, down_board_border, right_board_border, up_board_border,
+                 left_board_border):
         # Set board properties
         self.width = 192 * 10
         self.height = 108 * 10
@@ -21,14 +22,15 @@ class Board:
         # Set background properties
         self.background = pygame.image.load("Images/Boards/Plansza.png")
         self.background = pygame.transform.scale_by(self.background, 1)
-        self.background_x = self.camera_x - 960
-        self.background_y = self.camera_y - 540
+        # 960
+        self.background_x = self.camera_x - self.centering['x']
+        self.background_y = self.camera_y - self.centering['y']
 
         # Set border
-        self.border_x1 = border_x1
-        self.border_y1 = border_y1
-        self.border_x2 = border_x2
-        self.border_y2 = border_y2
+        self.down_border = down_board_border
+        self.right_border = right_board_border
+        self.up_border = up_board_border
+        self.left_border = left_board_border
 
     def update_camera_position(self, position_player_x, position_player_y):
         self.camera_x = position_player_x - self.camera_width // 2
