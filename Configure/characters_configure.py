c6bb4810cb8f36ea310297da14edde0def9e60b8
NecroGochi <necro.gochi@gmail.com>
2023-08-24 15:35:04 +0200

    Create characters_configure.py
    File contain the heroes parameters.

---
+++ Configure/characters_configure.py
@@ -0,0 +1,19 @@
+PATH_IMAGINES_Heroes = "Images/Heroes/"
+
+
+Necromancer = {
+    "Name": "Necromancer",
+    "Size": 100,
+    "HP": 49000,
+    "AP": 13,
+    "DP": 4,
+    "Speed": 9,
+    "Max_Exp": 1000,
+    "Bonus_level": {
+            'health': 3,
+            'attack': 5,
+            'defense': 2,
+            'speed': 1
+        },
+    "Images": [PATH_IMAGINES_Heroes + "nekromanta_1.png", PATH_IMAGINES_Heroes + "nekromanta_2.png"]
+}
