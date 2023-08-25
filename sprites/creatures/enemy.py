90e86b1fd76892bd9b20dc8f86f12c48aa263da2
NecroGochi <necro.gochi@gmail.com>
2023-08-25 15:39:13 +0200

    Update enemy.py
    Variable boss, hit, flip_image went outside __init__.

--- sprites/creatures/enemy.py
+++ sprites/creatures/enemy.py
@@ -4,22 +4,21 @@ import math
 
 
 class Enemy(Sprite):
+    boss = False
+    hit = True
+    flip_image = False
+
     def __init__(self, position_x, position_y, size, color, speed, attack, defense, health, experience, image):
         # Set player properties
         self.size = size
         self.color = color
         self.speed = speed
-        self.hit = True
         self.attack = attack
         self.defense = defense
         self.health_points = health
         self.actual_health_points = health
-        self.boss = False
         self.experience = experience
-
         self.image_sprite = pygame.image.load(image)
-        self.flip_image = False
-
         # Set initial player position
         self.hitbox = pygame.Rect(position_x, position_y, self.size, self. size)
 
