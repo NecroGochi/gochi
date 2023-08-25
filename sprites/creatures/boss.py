94f75683bbefd8914c59f633d68da27c501ee7a8
NecroGochi <necro.gochi@gmail.com>
2023-08-25 15:44:23 +0200

    change __init__ at Boss class
    Used super() to init the same variables from parent class:
    size
    image
    speed
    hit
    attack
    defense
    health_points
    actual_health_points
    experience
    image_sprite
    flip_image
    hitbox

--- sprites/creatures/boss.py
+++ sprites/creatures/boss.py
@@ -5,24 +5,9 @@ class Boss(Enemy):
     health_point_y_bar_size = 10
 
     def __init__(self, position_x, position_y, size, color, speed, attack, defense, health, experience, image):
-        # Set player properties
-        self.size = size
-        self.image = color
-        self.speed = speed
-        self.hit = True
-        self.attack = attack
-        self.defense = defense
-        self.health_points = health
-        self.actual_health_points = health
         self.boss = True
-        self.experience = experience
         self.final_boss = False
-
-        self.image_sprite = pygame.image.load(image)
-        self.flip_image = False
-
-        # Set initial player position
-        self.hitbox = pygame.Rect(position_x, position_y, self.size, self. size)
+        super().__init__(position_x, position_y, size, color, speed, attack, defense, health, experience, image)
 
     def render_hp_bar(self, window, color, board_camera_x, board_camera_y):
         pygame.draw.rect(window, color,
@@ -32,4 +17,3 @@ class Boss(Enemy):
 
     def is_final_boss(self):
         self.final_boss = True
-
