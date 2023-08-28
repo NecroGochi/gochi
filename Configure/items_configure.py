6f4edd5ecdec9b1503ec6b728e9c057de6f8a717
NecroGochi <necro.gochi@gmail.com>
2023-08-28 10:41:11 +0200

    Create items_configure.py
    Stores item data

---
+++ Configure/items_configure.py
@@ -0,0 +1,16 @@
+PATH_IMAGINES_ITEMS = "Images/Items/"
+
+
+Book = {
+    "Name": "Possessed book",
+    "Shape": "Rect",
+    "Distance_from_player": 150,
+    "Quantity": 1,
+    "Power": 20,
+    "Speed": 0.02,
+    "Bonus_level": {
+            'power': 2,
+            'speed': 0.02
+        },
+    "Images": [PATH_IMAGINES_ITEMS + "ksiazka.png"]
+}
