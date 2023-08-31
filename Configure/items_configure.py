49e5acaece0a49db294d9500fd77f04cfe785347
NecroGochi <necro.gochi@gmail.com>
2023-08-31 11:21:44 +0200

    add Card Weapon in items_configure.py

--- Configure/items_configure.py
+++ Configure/items_configure.py
@@ -14,3 +14,17 @@ Book = {
         },
     "Images": [PATH_IMAGINES_ITEMS + "ksiazka.png"]
 }
+
+Card = {
+    "Name": "Possessed card",
+    "Shape": "Rect",
+    "Distance_from_player": 300,
+    "Quantity": 1,
+    "Power": 20,
+    "Speed": 2.0,
+    "Bonus_level": {
+            'power': 5,
+            'speed': 0
+        },
+    "Images": [PATH_IMAGINES_ITEMS + "karta.png"]
+}
