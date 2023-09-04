54fef11f60c1bccd2b6f989a4263978d339a9967
NecroGochi <necro.gochi@gmail.com>
2023-09-04 11:47:15 +0200

    delete variables in main.py
    Deleted unused variables

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from Configure.configure import load_configure_data
 from Menu.main_menu import MainMenu
 
 
@@ -12,19 +11,9 @@ window_height = 108 * 5
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Gochi Game")
 
-# Set colors
-black = (0, 0, 0)
-white = (255, 255, 255)
-
 # Set Fonts
 font_title = pygame.font.Font(None, 64)
 font_options = pygame.font.Font(None, 32)
-
-configure_data = load_configure_data()
-
-# menu_state: main; lobby; option
-menu_state = 'main'
-selected_option = 0
 
 # Menus
 main_menu = MainMenu(window, window_width, window_height, font_title, font_options)
